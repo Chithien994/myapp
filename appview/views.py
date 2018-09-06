3bafafba4395706f6b0971e6a913f7511a609cf3
DESKTOP-VF0P4M8\Thien <chithien994@gmail.com>
2018-09-06 18:07:41 +0700

    update

--- appview/views.py
+++ appview/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from rest_framework.authtoken.models import Token
 
 def home(request):
     return render(request, 'frontend/home.html',{})
 
 def job(request):
-	return render(request, 'jobs/index.html',{})
+	token = Token.objects.first()
+	return render(request, 'jobs/index.html',{"token":token})
